f5c3d2a00ed9269f485f862b59ce37def90109b9
Oicirtap <patricionoyola@gmail.com>
2016-03-04 20:11:44 -0500

    added my scene and Scripts folder

---
+++ Assets/Scripts/CameraMovementPnoyola.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementPnoyola : MonoBehaviour {
+
+	public float speed = 0.001f;
+	public float turnSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		transform.Translate (Vector3.forward * moveVertical * speed);
+		transform.Rotate (Vector3.up * moveHorizontal * turnSpeed);
+
+	}
+}
