301b98b72b5de39dee0bde9db11cf50caea73ea2
Oicirtap <patricionoyola@gmail.com>
2016-03-06 22:47:22 -0500

    created new scene with Dungeon assets

---
+++ Assets/Scripts/LightPnoyola.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPnoyola : MonoBehaviour {
+
+	
+	private Light l;
+	
+	void Start () {
+		l = GetComponent<Light> ();
+	}
+	
+	void Update () {
+		if (Input.GetKey ("space")) {
+			l.intensity = 4f;
+		} else {
+			l.intensity = 0.1f;
+		}
+	}
+}
