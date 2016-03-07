a09483680310a558caaa7b94a8bc5f58aa7f61f8
patrick-lowe <plowe@mit.edu>
2016-03-07 09:05:02 -0500

    added prefabs

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTracking : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = target.position;
+        pos.y = gameObject.transform.position.y;
+        gameObject.transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FixedUpdate ()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 vel = new Vector3(h , 0, v);
+
+        vel = vel.normalized * speed;
+
+        rb.velocity = vel;
+        RotateToMouse();
+    }
+
+    void RotateToMouse()
+    {
+        Vector3 v3T = Input.mousePosition;
+        Debug.Log(v3T);
+        v3T.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+        v3T = Camera.main.ScreenToWorldPoint(v3T);
+        transform.LookAt(v3T);
+    }
+}
