190adc22ad03781b6e2ed8a7bbc079de869abe8b
DavidBocek <ddperera@mit.edu>
2016-03-09 13:57:21 -0800

    destroy the player when they reach the exit

---
+++ Assets/Scripts/EndPointBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndPointBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider coll)
+	{
+		Debug.Log(coll.gameObject.tag);
+		if(coll.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("Victory!");
+			GameObject.Destroy(coll.gameObject);
+		}
+
+	}
+}
