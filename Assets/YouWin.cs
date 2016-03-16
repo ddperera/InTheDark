566a0d2e7e30d052e52e0c83a289a5bd1fea18cf
Oicirtap <patricionoyola@gmail.com>
2016-03-16 01:27:29 -0400

    added you win text at level 3

---
+++ Assets/YouWin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YouWin : MonoBehaviour {
+	private GameObject player;
+	private PlayerInventory playerInventory;
+	private bool finished = false;
+
+	private TextManager textM;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerInventory = player.GetComponent<PlayerInventory> ();
+
+		textM = GameObject.FindGameObjectWithTag ("Text").GetComponent<TextManager> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject == player && !finished) {
+			finished = true;
+			textM.UpdateText ("You made it to the end of the Game! You Win!");
+		}
+	}
+}
